714fc633d491d20b6c251ef54d7cb8c1fad01869
Arcadius Kazimierski Javascript, Node.js, MongoDB, Angular.js, Express.js, C# .Net, SQL Server, Java, Android <arcadiusk@gmail.com>
2016-02-29 04:48:13 -0500

    fix: Data received

--- Wearable/src/main/java/com/example/android/wearable/datalayer/DataLayerListenerService.java
+++ Wearable/src/main/java/com/example/android/wearable/datalayer/DataLayerListenerService.java
@@ -40,7 +40,7 @@ public class DataLayerListenerService extends WearableListenerService {
     private static final String TAG = "DataLayerListenerServic";
 
     private static final String START_ACTIVITY_PATH = "/start-activity";
-    private static final String DATA_ITEM_RECEIVED_PATH = "/data-item-received";
+    private static final String DATA_ITEM_RECEIVED_PATH = "data/data-item-received";
     public static final String COUNT_PATH = "/count";
     public static final String IMAGE_PATH = "images/image";
     public static final String IMAGE_KEY = "images/photo";
